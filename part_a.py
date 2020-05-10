18553e77e60505f7e124e4c5a151fef612639582
cngo <Hieu_Ngo@intuit.com>
2020-05-10 10:26:55 -0600

    partA

--- part_a.py
+++ part_a.py
@@ -13,21 +13,36 @@ def get_connected_components(graphframe):
     # For example, [[a_1, a_2, ...], [b_1, b_2, ...], ...]
     # then a_1, a_2, ..., a_n lie in the same component,
     # b_1, b2, ..., b_m lie in the same component, etc
-    return [[]]
-
+    # return [[]]
+    components_map = {}
+    results_df = graphframe.connectedComponents()
+    results = results_df.rdd.collect()
+    for v in results:
+        component = v["component"]
+        if not (component in components_map):
+            components_map[component] = []
+        components_map[component].append(v["id"])
+    return list(components_map.values())
 
 if __name__ == "__main__":
     vertex_list = []
     edge_list = []
     with open('dataset/graph.data') as f:  # Do not modify
         for line in f:
-            src = None  # TODO: Parse src from line
-            dst_list = []  # TODO: Parse dst_list from line
+            vertices = line.split(" ")
+            src = vertices[0]
+            dst_list = [v.strip() for v in vertices[1:]]
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create vertices dataframe
-    edges = spark.createDataFrame(...)  # TODO: Create edges dataframe
+    vertices = spark.createDataFrame(
+        vertex_list,
+        ["id"]
+    ) 
+    edges = spark.createDataFrame(
+        edge_list,
+        ["src", "dst"]
+    )
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/connected-components")
